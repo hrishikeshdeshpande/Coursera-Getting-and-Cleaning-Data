a30722c0bd837e80b529b093941a6fc6310749d5
hrishikeshdeshpande <hrishikesh.12feb@gmail.com>
2015-05-22 00:10:56 +0530

    md files

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-##download.file(fileUrl,destfile="./data/Dataset.zip")
+download.file(fileUrl,destfile="./data/Dataset.zip")
 unzip(zipfile="./data/Dataset.zip",exdir="./data")
 path_rf <- file.path("./data" , "UCI HAR Dataset")
 files<-list.files(path_rf, recursive=TRUE)
